5f1f309bb4568809f4b170f5f867807eecb8d62e
apapiez <alex.papiez@hotmail.com>
2017-04-18 20:12:59 +0100

    GUYS PLZ

---
+++ Alex/timedict.py
@@ -0,0 +1,28 @@
+"""
+Holds words corresponding to time values
+"""
+hourdict = {0:"Twelve", 1:"One", 2:"Two",
+            3:"Three", 4:"Four", 5:"Five",
+            6:"Six", 7:"Seven", 8:"Eight",
+            9:"Nine", 10:"Ten", 11:"11", 12:"12",
+            13:"One", 14:"Two", 15:"Three", 16:"Four",
+            17:"Five", 18:"Six", 19:"Seven", 20:"Eight",
+            21:"Nine", 22:"Ten", 23:"11"}
+            
+a_dict = {2:"Twenty", 3:"Thirty", 4:"Fourty",5:"Fifty"}
+
+b_dict = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six",
+          7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten", 11:"Eleven", 12:"Twelve",
+          13:"Thirteen", 14:"Fourteen", 15:"Fifteen", 16:"Sixteen", 17:"Seventeen",
+          18:"Eighteen", 19:"Nineteen"}
+          
+w_dict = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine",
+          10:"Ten", 11:"Eleven", 12:"Twelve", 13:"Thirteen", 14:"Fourteen", 15:"Fifteen", 16:"Sixteen",
+          17:"Seventeen", 18:"Eighteen", 19:"Nineteen", 20:"Twenty", 21:"Twenty one", 22:"Twenty two",
+          23:"Twenty three", 24:"Twenty Four", 25:"Twenty five", 26:"Twenty six", 27:"Twenty Seven",
+          28:"Twenty eight", 29:"Twenty nine", 30:"Thirty", 31:"Thirty one", 32:"Thirty two", 33:"Thirty three",
+          34:"Thirty four", 35:"Thirty five", 36:"Thirty six", 37:"Thirty seven", 38:"Thirty eight", 39:"Thirty nine",
+          40:"Fourty", 41:"Fourty one", 42:"Fourty two", 43:"Fourty three", 44:"Fourty four", 45:"Fourty five",
+          46:"Fourty six", 47:"Fourty seven", 48:"Fourty eight", 49:"Fourty nine", 50:"fifty", 51:"fifty one", 
+          52:"fifty two", 53:"fifty three", 54:"fifty four", 55:"fifty five", 56:"fifty six", 57:"fifty seven",
+          58:"fifty eight", 59:"fifty nine", 0:""}
